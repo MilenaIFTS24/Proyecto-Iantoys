25bd21e66448e036a6731aac76917d1cd3c4d366
Milena Lingua <39387464@ifts24.edu.ar>
2026-07-21 15:17:41 -0300

    Boceto de image.model.js con multer.

--- backend/models/imagen.model.js
+++ backend/models/imagen.model.js
@@ -1,6 +1,8 @@
-export class Imagen {
-    constructor(id, url) {
+import Multer from "multer";
+
+export class Archivo extends Multer.File {
+    constructor(id, fieldname, originalname, encoding, mimetype, buffer, size, destination, filename, path) {
+        super(fieldname, originalname, encoding, mimetype, buffer, size, destination, filename, path);
         this.id = id;
-        this.url = url;
     }
 }
